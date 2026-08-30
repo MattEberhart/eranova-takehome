bbb27b2537572e75593d712929775d2dfa92f42e
Matt Eberhart <carlmattheweberhart@gmail.com>
2026-08-30 11:03:03 -0400

    still importing models.invoice wrong. beyond top level directory

--- backend/services/invoice_service.py
+++ backend/services/invoice_service.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 import uuid
-from ..models.invoice import InvoiceMetadata, InvoiceStatus
+from models.invoice import InvoiceMetadata, InvoiceStatus
 
 class InvoiceService:
     def init(self):
