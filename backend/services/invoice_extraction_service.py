adedaba441fa560a95d532c0b98745d7d247855d
Matt Eberhart <carlmattheweberhart@gmail.com>
2026-09-01 16:12:36 -0400

    Was doing the float to decimal dynamo db thing wrong

--- backend/services/invoice_extraction_service.py
+++ backend/services/invoice_extraction_service.py
@@ -3,6 +3,7 @@ import os
 from models.invoice_document import InvoiceDocumentMetadata, InvoiceStatus
 from models.invoice_extraction import InvoiceExtraction
 from decimal import Decimal
+import json
 
 class InvoiceExtractionService:
     def __init__(self):
@@ -13,7 +14,6 @@ class InvoiceExtractionService:
     def create_invoice_extraction(
         self,
         invoice_extraction:InvoiceExtraction) -> InvoiceExtraction:
-    
-        self.extraction_table.put_item(
-        Item=invoice_extraction.model_dump(mode="json", parse_float=Decimal,)
-    )
+        item = json.loads(invoice_extraction.model_dump_json(), parse_float=Decimal)
+        self.extraction_table.put_item(Item=item)
+        return invoice_extraction
