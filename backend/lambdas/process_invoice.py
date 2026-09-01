d9b80799fc9bee6ba925a5884203420920cf3b84
Matt Eberhart <carlmattheweberhart@gmail.com>
2026-09-01 11:36:55 -0400

    Want to mark it failed on agent failures. Returning 200 no matter what to avoid costly retries

--- backend/lambdas/process_invoice.py
+++ backend/lambdas/process_invoice.py
@@ -2,6 +2,10 @@ from services.invoice_service import InvoiceService
 from models.invoice_document import InvoiceStatus
 import json
 from agents.invoice_processor import invoice_processor
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 service = InvoiceService()
 
@@ -18,24 +22,35 @@ def handler(event, context):
 
     thread_id = f"invoice:{invoice_id}" # This means we can only process/ have one thread per upload. Maybe not great, something to notes.
 
-    result = invoice_processor.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": f"Process invoice {invoice_id}"
+    try:
+        result = invoice_processor.invoke(
+            {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": f"Process invoice {invoice_id}"
+                    }
+                ]
+            },
+            config={
+                "configurable": {
+                    "thread_id": thread_id
                 }
-            ]
-        },
-        config={
-            "configurable": {
-                "thread_id": thread_id
             }
-        }
-    )
+        )
+    except Exception:
+        logger.exception("Invoice processing agent failed.")
+        service.mark_invoice_processing(
+            invoice_id=invoice_id,
+            status=InvoiceStatus.FAILED
+        )
+        # Not re raisng. Wasting tokens / money on retries. Will just return 200 for nwo
+        #raise ## re throws. Causes lambda to retry the S3 event.
+                ## Not sure we need the retry, so far retries are finding same failure path
+                ## Maybe retries good when we get passed code issues and get to agentic decisions
 
     return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
-            "body": "Moved to Processing"
+            "body": f"Invoice processed"
         }
